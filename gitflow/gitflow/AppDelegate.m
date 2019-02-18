6af2535a86d5e114ad3e87aec70310a4cb86d01f
dev <77603901@qq.com>
2019-02-19 02:22:13 +0800

    B

--- gitflow/gitflow/AppDelegate.m
+++ gitflow/gitflow/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+//    A
     // Override point for customization after application launch.
     return YES;
 }
